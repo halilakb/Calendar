d3cb3d73356c8baf9457ec6f21c98a2c5cd83cf0
akbuluth15 <50354100+akbuluth15@users.noreply.github.com>
2019-05-14 21:09:51 +0300

    Update calendar.h

--- calendar.h
+++ calendar.h
@@ -4,15 +4,15 @@
 #include <iostream>
 using namespace std;
 
-class Calendar{
-	Event *head;
-	Event *tail;
+class Calendar{				// calendarclass is used for making linklist
+	Event *head;			// head pointer of linklist, pointing first element of the list
+	Event *tail;			// tail pointer of linklist, pointing last element of the list
 public:
-	Calendar() {head = NULL; tail = NULL;}
-	Event* addEvent(int,int,int,int,int);
-	void deleteEvent(int);
-	void listEvents();
-	void filterEvents(int,int);
+	Calendar() {head = NULL; tail = NULL;}		// constructor of Calendar
+	Event* addEvent(int,int,int,int,int);		// method for adding elements to the list
+	void deleteEvent(int);				// method for deleting elements from the list
+	void listEvents();				// method for listing the avaliable elements
+	void filterEvents(int,int);			// method for listing the avaliable elements with a certain year and month
 };
 
 #endif
